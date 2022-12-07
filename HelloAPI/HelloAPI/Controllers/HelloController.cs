42620bc78b6e599a3e4440a526b97aeeb1226df0
Brad Podmore <brad.podmore@integration.works>
2022-12-07 15:19:14 +1300

    Added API source code.

---
+++ HelloAPI/HelloAPI/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAPI.Controllers;
+
+[ApiController]
+[Route("api/hello")]
+public class HelloController : Controller
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello!";
+    }
+
+    [HttpGet("{name}")]
+    public string Get(string name)
+    {
+        return $"Hello {name}!";
+    }
+}
